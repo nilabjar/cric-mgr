202b03b91dca74712250bfdba4cc3b19ffa3b72f
Nilabja Roy <nilabjar@nilabjar-HP-ProBook-4720s.(none)>
2012-06-01 18:12:27 -0500

    Added second team innings: results, now a team is winning/losing

--- CricketManager/src/org/cricket/manager/Match.java
+++ CricketManager/src/org/cricket/manager/Match.java
@@ -17,42 +17,123 @@ public class Match {
 	private int current_wickets = 0;
 	private int inningsRun = 0;
 	
+	private int firstTeamRun = 0;
+	private int secondTeamRun = 0;
+	
+	private int currentOver = 0;
+	
+	private boolean chasing = false;
 	
 	void startPlay () {
+		
+		// toss 
+		
+		// first innings ....
+		
+		System.out.println ("Starting First Innings");
+		startInnings (first);
+		
+		this.firstTeamRun = this.inningsRun;
+		//second innings ... 
+		
+		System.out.println ("\nInnings Over : Total " + firstTeamRun);
+		
+		reset ();
+		
+		System.out.println ("\nStarting Second Innings: Target " + (firstTeamRun + 1));
+		Sleep (10000);
+		
+		chasing = true;
+		startInnings (second);
+
+		this.secondTeamRun = this.inningsRun;
+		// result ....
+		
+		System.out.println((firstTeamRun > secondTeamRun) ? "First Team Wins" : "Second Team Wins");
+		
+		//matchResult ();
+		
+
+	}
+	
+	private void reset() {
+		// TODO Auto-generated method stub
+		current_wickets = 0;
+		inningsRun = 0;
+		currentOver = 0;
+	}
+
+	private void startInnings(Team team) {
+		// TODO Auto-generated method stub
 		//bowler = second.getNextBowler ();
 		striker = new Batsman ();
 		
 		for (int i = 0;i < 20;i++) {
+			currentOver++;
+			print ("\n");
 			System.out.println("Starting over number " + (i+1));
+			print ("\n");
 			if (this.startOver() < 0)
 				return;
+			
+			Sleep (1000);
 		}
+		
 	}
 	
+	public void Sleep (int ms) {
+		try {
+			Thread.sleep(ms);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	int startOver () {
 		for (int i = 0;i < 6;i++) {
 			Shot s = striker.bat(null);			
-			System.out.printf("It is %s%n", s.result);
+			System.out.println(currentOver + "." + (i+1) + " : " + s.result);
 			
 			// analyze result ....
-			analyzeResult(s);
-			
-			System.out.println("The score is " + 
-								this.inningsRun + 
-								" for " + 
-								this.current_wickets);
+			analyzeResult(s);			
 			
 			if (current_wickets == 10)
 				return -1;
-		}	
+			if (chasing) {
+				if (this.inningsRun > firstTeamRun)
+					return -1;
+			}
+		}
+				
+		printScore ();
 		return 0;
+	}
+	
+	private void printScore () {
+		print ("\n");
+		
+		System.out.println("The score is " + 
+				this.inningsRun + 
+				" for " + 
+				this.current_wickets +
+				" in " + 
+				this.currentOver + 
+				" overs " + 
+				(chasing ? ("Chasing " + this.firstTeamRun) : ""));
+		
+		print ("\n");
+	}
+	
+	private void print (String s) {
+		System.out.println (s);
 	}
 
 	private void analyzeResult(Shot s) {
 		// TODO Auto-generated method stub
 		switch (s.result) {
 		case OUT:
-			this.current_wickets++;
+			out ();
 			break;
 		case SINGLE:
 			this.inningsRun++;
@@ -70,5 +151,16 @@ public class Match {
 			this.inningsRun += 6;
 			break;
 		}
+	}
+
+	private void out () {
+		this.current_wickets++;
+		
+		//Sleep (5000);
+		
+		//print ("\nNew batsman walks in : Play Resumes");
+		
+		//printScore ();
+		//Sleep (10000);
 	}
 }
